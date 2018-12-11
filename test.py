574e13a8fc55d55e49b1e04dae00e7240230dbe3
wqy <wqy9619@mail.ustc.edu.cn>
2018-12-11 21:26:51 +0800

    first commit

--- test.py
+++ test.py
@@ -1,10 +1,6 @@
 import sys
 sys.path.append('build')
-from get_mesh import *
+from get_dr import *
 import numpy as np
 import time
-#a= time.time()
-feature = np.fromfile('/raid/jzh/RimdFeature1002/Feature1/face_38_1002.dat')
-a=time.time()
-m = get_mesh('/raid/jzh/FeatureDistangle/data/distangle/Mean_Face.obj', feature)
-print('Time cost {}'.format(time.time()-a))
+print(get_dr('1_new.obj', '2_new.obj'))
