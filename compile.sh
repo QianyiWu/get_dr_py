e8a0e7dc934a301e7910b6a02f3dc71993697b28
kychern <kychern@outlook.com>
2019-02-26 16:11:30 +0800

    modifications for released version

--- compile.sh
+++
@@ -1,3 +0,0 @@
-c++ -O3 -w -shared -std=c++11 -fPIC `python3 -m pybind11 --includes` get_dr.cpp -o get_dr`python3-config --extension-suffix` -I/usr/include/eigen3 -I/home/qianyi/get_mesh_py/pybind11 -L/usr/local/lib -lOpenMeshCore -lOpenMeshTools
-
-
